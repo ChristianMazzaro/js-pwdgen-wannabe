7ea9a2b497d924a84f87fc9e2c48a5c8df59dd3b
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-06-09 17:56:38 +0200

    Esercizio completato

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+    const FirstName = prompt('Inserire Nome Utente');
+    console.log(FirstName);
+
+    const LastName = prompt('Inserire Cognome Utente');
+    console.log(LastName);
+
+    const ColorePreferito = prompt('Inserire Colore Preferito Utente');
+    console.log(ColorePreferito);
+
+    document.writeln( FirstName, LastName, ColorePreferito, '101');
